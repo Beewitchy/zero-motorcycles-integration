7415eff3b1aeffce5c1f6f2039fbe1f7eba63ca2
Ellie Hayward <ellie@witchymail.com>
2025-07-16 00:46:06 +0100

    linted

--- custom_components/zero_motorcycles_integration2/binary_sensor.py
+++ custom_components/zero_motorcycles_integration2/binary_sensor.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Any, cast
+from typing import cast
 
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
